9e5913a4f3b13993d318351699ee1986ac36b4a3
justseny17 <seanrice872@gmail.com>
2021-03-19 00:55:18 -0400

    Add files via upload

---
+++ javaSockets/src/javaSockets/Server.java
@@ -0,0 +1,76 @@
+package javaSockets;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	
+	private ServerSocket serverObject = null;
+	private Socket newSocket1 = null;
+        private Socket newSocket2 = null;
+        private DataInputStream clientInput1 = null;
+        private DataInputStream clientInput2 = null;
+        private DataOutputStream toClient2 = null;
+        private DataOutputStream toClient1 = null;
+        
+	public static void main(String[] args) {
+		Server serverObject = new Server(8000);
+	}
+	
+	public Server(int port)
+    {
+        try
+        {
+            serverObject = new ServerSocket(port);
+            
+            System.out.println("I am the TCP Server. I am listening on port " + port + ", and waiting for a client.");
+            
+            newSocket1 = serverObject.accept();
+            toClient1 = new DataOutputStream(new BufferedOutputStream(newSocket1.getOutputStream()));
+            System.out.println("I received a connection from a client.");
+            clientInput1 = new DataInputStream(new BufferedInputStream(newSocket1.getInputStream()));
+            
+            System.out.println("I am waiting for a second client.");
+            
+            newSocket2 = serverObject.accept();
+            toClient2 = new DataOutputStream(new BufferedOutputStream(newSocket2.getOutputStream()));
+            System.out.println("I received a connection from a second client.");
+            clientInput2 = new DataInputStream(new BufferedInputStream(newSocket2.getInputStream()));
+            
+            String clientData1 = "void";
+            String clientData2 = "void";
+            
+            while (!clientData1.equals("exit") && (!clientData2.equals("exit")))
+            {
+                try
+                {
+                    clientData1 = clientInput1.readUTF();      // Unicode Translation Format
+                    System.out.println("Client 1 has written: ");
+                    System.out.println(clientData1);
+                    toClient2.writeUTF("Client 1 has written: \n" + clientData1);
+                    
+                    clientData2 = clientInput2.readUTF();
+                    System.out.println("Client 2 has written: ");
+                    System.out.println(clientData2);
+                    toClient1.writeUTF("Client 2 has written: \n" + clientData2);
+  
+                }
+                catch(IOException io)
+                {
+                    System.out.println(io);
+                }
+            }
+            
+            System.out.println("The client has asked to leave.");
+            System.out.println("I am now closing the socket connection.");
+            
+            newSocket1.close();
+            newSocket2.close();
+        }
+        catch(IOException io)
+        {
+            System.out.println(io);
+        }
+    }
+        
+}
---
+++ javaSocketsClient/src/javaSocketsClient/Client.java
@@ -0,0 +1,87 @@
+package javaSocketsClient;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Client {
+    
+	private Socket newSocket = null;
+        private DataOutputStream toServer = null;
+        private DataInputStream fromServer = null;
+	
+	public static void main(String[] args) {
+            //
+            String sel;
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter an address to connect to."
+                    + "\n(Hit Enter to try localhost)");
+            sel = scan.nextLine();
+            if (sel.equals("")) {
+                    sel = "127.0.0.1";
+            }
+            System.out.println("Connecting to " + sel + ":" + 8000);
+            //
+		Client client = new Client(sel, 8000);
+	}
+	
+	public Client(String address, int port)
+    {
+        try
+        {
+        	System.out.println("I am the TCP Client. I am about to establish a connection with the server.");
+        	
+        	newSocket = new Socket(address, port);
+                
+            toServer = new DataOutputStream(newSocket.getOutputStream());
+            fromServer = new DataInputStream(new BufferedInputStream(newSocket.getInputStream()));
+            
+            System.out.println("I am now connected to the server.");
+        }
+        catch(UnknownHostException un)
+        {
+            System.out.println("Connection failed:");
+            System.out.println(un);
+        }
+        catch(IOException io)
+        {
+            System.out.println("Connection failed:");
+            System.out.println(io);
+        }
+        
+        String clientData = "void";
+        String serverData = "void";
+        
+        Scanner scan = new Scanner(System.in);
+        
+        while (!clientData.equals("exit"))
+        {
+            try
+            {
+            	System.out.println("Enter data that you want me to send to the server: ");
+//                Scanner scan = new Scanner(System.in);
+            	clientData = scan.nextLine();
+            	toServer.writeUTF(clientData);
+                System.out.println("awaiting response");
+                serverData = fromServer.readUTF();
+                System.out.println(serverData);
+                
+            }
+            catch(IOException io)
+            {
+                System.out.println(io);
+            }
+        }
+        
+        try
+        {
+            toServer.close();
+            newSocket.close();
+        }
+        catch(IOException io)
+        {
+            System.out.println(io);
+        }
+    }
+
+}
